cb3c707a1c9cae32fff64de20bc922dc09ae38aa
Kacper Kielak <kacper.kielak@featurespace.co.uk>
2020-03-10 18:09:07 +0000

    Add eager execution to the debug mode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+import tensorflow as tf
 
 from dle.attention import run_attention
 from dle.utils.logging import setup_logging
@@ -8,6 +9,8 @@ from dle.utils.logging import setup_logging
 @click.option('--debug', is_flag=True)
 def cli(debug: bool):
     setup_logging(debug)
+    if debug:
+        tf.config.experimental_run_functions_eagerly(True)
 
 
 cli.add_command(run_attention)
